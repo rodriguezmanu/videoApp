f1549f0b564125dc6cd6d3726bc0f394668b00db
Manuel Rodriguez <rodriguez.manu@gmail.com>
2016-07-12 20:40:14 -0300

    adding task to deploy heorku

--- Gruntfile.js
+++ Gruntfile.js
@@ -633,4 +633,5 @@ module.exports = function(grunt) {
   grunt.registerTask('default', ['newer:jshint', 'test', 'build']);
 
   grunt.registerTask('lint', ['jscs:all', 'jscs:app', 'jshint:all']);
+  grunt.registerTask('deploy-prod', ['clean', 'ngconstant:prod', 'build']);
 };
